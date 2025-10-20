c092ed74e67a325fd18648d2203685ac6202a1bf
jjy12163 <lalalove0310@gmail.com>
2025-10-20 17:15:11 +0900

    Fix: delimiter 리스트를 기준으로 배열 생성이 되지 않던 문제 해결

--- src/main/java/calculator/domain/SplitString.java
+++ src/main/java/calculator/domain/SplitString.java
@@ -7,13 +7,14 @@ import static calculator.domain.UpdateDelimiterList.*;
 
 public class SplitString {
 
-    String[] numArr;
-    ArrayList<Integer> intNumArr = new ArrayList<>();
+    public static ArrayList<Integer> SplitString(String input) {
 
-    public ArrayList<Integer> SplitString(String input) {
+        String[] numArr = new String[0];
+        ArrayList<Integer> intNumArr = new ArrayList<>();
 
         try {// 구분자 리스트
             String[] delimiterList;
+            String delimiterListStr;    // delimiterList에 있는 구분자를 "|"로 합함
             delimiterList = UpdateDelimiterList(input);
 
             if (isExistCustomDelimiter(input)) {  // custom 구분자가 있을 경우
@@ -21,9 +22,9 @@ public class SplitString {
             }
 
             // delimiter대로 input을 split - num만 남음
-            for (String delimiter : delimiterList) {
-                numArr = input.split(delimiter);
-            }
+            delimiterListStr = String.join("|", delimiterList);
+
+            numArr = input.split(delimiterListStr);
 
             // num에 문자열 남아있는 경우 - 에러 발생
             for(String num : numArr) {
