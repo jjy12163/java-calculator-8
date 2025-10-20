5bb6a0df6a8fa9e97e56241bdb0bbb0fba669920
jjy12163 <lalalove0310@gmail.com>
2025-10-20 18:17:44 +0900

    Feat: readline으로 문자열 입력, 공백이라면 0 으로 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import calculator.domain.Calculate;
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine(); // 한줄 모두 입력받음'
+        int result;
+        if(input.equals("")) {
+            result = 0;
+        } else {
+            result = Calculate.Calculate(input);
+        }
+        System.out.println("결과 : " + result);
     }
 }
